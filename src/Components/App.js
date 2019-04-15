73f2f88b2fb258ede5cf769e0fd737777adc0f5d
Jakub <jakub.bulhak@gmail.com>
2019-04-15 13:04:39 +0200

    Fetching data from API about bikes in stations

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Doughnut } from "react-chartjs-2";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get("http://localhost:8080/api/bikes/now")
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(`Wystąpił błąd: ${error}`);
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div>Test</div>;
   }
 }
 
