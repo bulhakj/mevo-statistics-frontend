49c21a774b596cb46a308aeef75fa61239780961
Jakub <jakub.bulhak@gmail.com>
2019-04-15 14:59:58 +0200

    Created component drawing chart about bicycles in stations with their percentage range.

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,21 +2,42 @@ import React, { Component } from "react";
 import { Doughnut } from "react-chartjs-2";
 import axios from "axios";
 import "./App.css";
+import DougnutChart from "./Chart";
 
 class App extends Component {
+  state = {
+    nowResponse: []
+  };
+
   componentDidMount() {
     axios
       .get("http://localhost:8080/api/bikes/now")
       .then(response => {
-        console.log(response.data);
+        console.log(`response ${response.data.length}`);
+        this.handleResponseNow(response.data);
+        this.handleMapBatteries(response.data);
+        this.handleFilterLowBatteries(this.state.allBatteries);
+        this.handleFilterMediumBatteries(this.state.allBatteries);
+        this.handleFilterHighBatteries(this.state.allBatteries);
       })
       .catch(error => {
         console.log(`Wystąpił błąd: ${error}`);
       });
   }
 
+  handleResponseNow = response => {
+    this.setState({
+      nowResponse: response
+    });
+  };
+
   render() {
-    return <div>Test</div>;
+    return (
+      <div>
+        <div>Test</div>
+        <DougnutChart nowResponse={this.state.nowResponse} />
+      </div>
+    );
   }
 }
 
---
+++ src/Components/Chart.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+
+const DougnutChart = props => {
+  const handleMapBatteries = props.nowResponse.map(item => item.battery);
+
+  const handleFilterLowBatteries = handleMapBatteries.filter(
+    battery => battery <= 40 && battery > 0
+  );
+
+  const handleFilterMediumBatteries = handleMapBatteries.filter(
+    battery => battery <= 70 && battery > 40
+  );
+
+  const handleFilterHighBatteries = handleMapBatteries.filter(
+    battery => battery > 70
+  );
+
+  const data = {
+    labels: [
+      `0% - 40%: ${handleFilterLowBatteries.length}`,
+      `41% - 69%: ${handleFilterMediumBatteries.length}`,
+      `70% - 100%: ${handleFilterHighBatteries.length}`
+    ],
+    datasets: [
+      {
+        data: [
+          handleFilterLowBatteries.length,
+          handleFilterMediumBatteries.length,
+          handleFilterHighBatteries.length
+        ],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
+      }
+    ]
+  };
+  return <Doughnut data={data} />;
+};
+
+export default DougnutChart;
